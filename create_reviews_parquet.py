06a1fc0c4a25fd2a7f23aa604a9b46a041d91b6f
Luisa Mercado <42242797+Lmercadom@users.noreply.github.com>
2021-11-27 22:35:20 -0800

    read restaurant data from s3

--- create_reviews_parquet.py
+++ create_reviews_parquet.py
@@ -17,14 +17,18 @@ def main(business_file, reviews_file, outdir):
     ])
 
     # Read business ids from business table
-    business_ids= spark.read.parquet(business_file).select('business_id')
+    business_ids= spark.read.parquet('s3://galv/restaurants.parquet/').select('business_id')
     # Read reviews json
     all_reviews = spark.read.json(reviews_file, schema=reviews_schema).repartition(16)
     # Keep reviews liked to business ids of interest
-    filtered_reviews = all_reviews.join(business_ids, on='business_id')
+    filtered_reviews = all_reviews.join(broadcast(business_ids), on='business_id')
     filtered_reviews = filtered_reviews.select('review_id', 'user_id', 'business_id', 'stars', 'useful', functions.col('text').alias('contents'), 'date')
 
-    filtered_reviews.write.parquet(outdir + '/reviews.parquet')
+    filtered_reviews.write.mode("overwrite").parquet(outdir + '/reviews.parquet')
+
+    utils.upload_files_to_s3(output, BUCKET)
+
+
 if __name__ == '__main__':
     business_file = sys.argv[1]
     reviews_file = sys.argv[2]
