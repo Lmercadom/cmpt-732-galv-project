0f2b7e8a0ab3610f4a207d56ce501a2d3c377a18
vishakha <connect.with.vishakha@gmail.com>
2021-12-09 05:40:09 -0800

    CLI changes to incorporate backend changes

--- app/main.py
+++ app/main.py
@@ -1,29 +1,26 @@
 import argparse
 import logging
-import pandas as pd
 
-# local imports
 import api
 import utils
 
 if __name__ == '__main__':
     # TODO remove defaults
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path", help="path to file", default="joined_sample.csv")
+    parser.add_argument("--path", help="path to file", default="")
     parser.add_argument("--coordinates", help="coordinates as tuple of floats", default=None)
-    parser.add_argument("--location_str", help="location as string", default="toronto")
+    parser.add_argument("--location_str", help="location as string", default="columbus")
     parser.add_argument("--attributes", help="cuisine types sepearated by ','", default="dosa")
     parser.add_argument("--threshold", help="distance from your location", default=10.0)
     args = parser.parse_args()
 
     logger = utils.create_logger(logging.DEBUG)
     logging.info("Starting the app")
-    # df = pd.read_csv(args.path)
     api.get_similar_business(
         coordinates=args.coordinates,
         location_str=args.location_str,
         attributes=args.attributes,
-        df=df,
+        path=args.path,
         threshold=float(args.threshold)
     )
 
