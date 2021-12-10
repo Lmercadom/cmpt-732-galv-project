ac564e06b9b05649a127d513f0b814af89d8b90d
Luisa Mercado <42242797+Lmercadom@users.noreply.github.com>
2021-12-09 22:57:22 -0800

    updated get_reviews_ngram_counts.py
    -Save to file instead of plotting
    -Cleaned up file
    - Added comments

--- get_reviews_ngram_counts.py
+++ get_reviews_ngram_counts.py
@@ -9,11 +9,6 @@ import string
 import re
 import nltk
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk import FreqDist
-from matplotlib import pyplot as plt
-import numpy as np
-import pandas as pd
 from nltk.tokenize import sent_tokenize
 from afinn import Afinn
 import sparknlp
@@ -24,23 +19,36 @@ from pyspark.ml import Pipeline
 af = Afinn()
 
 stop_words = stopwords.words('english')
-regex = re.compile('[' + re.escape(string.punctuation) + '0-9\\r\\t\\n]')
 
 def convert_rating(rating):
-    # Everything greater or equal to 4 is "positive"
-    # Else " Negative"
+    '''
+    Converts start ratings into "positive" or "negative"
+    rating >= 4 is "positive"
+    rating < 4 is "negative"
+    '''
+
     if rating >=4:
         return 1
     else:
         return 0
 
 def tokenize_text(text):
-    # break down reviews into sentences and join them with ;
+    '''
+    Breaks out review text into group of sentences
+    '''
     sentence_tokens = sent_tokenize(text)
     sentence_tokens = ';'.join(sentence_tokens)
     return sentence_tokens
 
 def assign_sentiment(sentence):
+    '''
+    assigns overall positive, negative and neutral sentiment scores
+    to sentences based on the Affin Lexicon
+    positive scores are positive sentiments
+    zero scores are neutral
+    negative scores are negative sentiments
+
+    '''
     sentiment_score = af.score(sentence)
     if sentiment_score > 0:
         return 1
@@ -49,16 +57,39 @@ def assign_sentiment(sentence):
     else:
         return 0
 
-
+# convert functions to spark udf
 stars_conveter = udf(lambda x : convert_rating(x))
 get_language = udf(lambda x : detect(x))
 get_sentence_tokens = udf(lambda x: tokenize_text(x))
 get_sentence_sentiments = udf(lambda x: assign_sentiment(x))
 
-def main(reviews_file, business_id, ngrams, outdir):
+def main(reviews_file, businesses, ngrams, outdir):
+    '''
+    Generates a count table for ngrams found in reviews
+    grouped by business id, review ratings and sentence sentiment
+
+    inputs:
+    reviews_file: The file containing the reviews
+    businesses: a txt file with businesses' ids
+    ngrams: the lenght of the ngrams
+    outdir: output directory
+
+    outputs:
+    table of ngrams and their frequency in csv
+    '''
+    # Get the business ids
+    with open(businesses) as f:
+        business_ids= f.read().split('\n')
+
+
     # 1) Get relevant columns from review data
-    reviews_df = spark.read.parquet(reviews_file).select('review_id','contents', 'stars', 'business_id')
-    reviews_df = reviews_df.filter(reviews_df['business_id'] == str(business_id))
+    reviews_df = spark.read.parquet(reviews_file).select('review_id', \
+                                                        'contents', \
+                                                        'stars', \
+                                                        'business_id').repartition(32)
+    reviews_df = reviews_df.filter(reviews_df['business_id'].isin(business_ids))
+
+
 
     #2) Filter out non English Reviews
     reviews_df = reviews_df.withColumn('language', get_language(reviews_df['contents']))
@@ -66,16 +97,24 @@ def main(reviews_file, business_id, ngrams, outdir):
 
     #3) Convert star reviews into positive and not negative reviews
     #   stars >= 4 are good, stars < 4 are not good
-    reviews_df = reviews_df.select('review_id', 'contents', stars_conveter(reviews_df['stars']).alias('rating')).cache()
-    # print(reviews_df.groupBy('sentiment').count().show(5))
+    reviews_df = reviews_df.select('review_id', 'contents', \
+                                    stars_conveter(reviews_df['stars']).alias('rating'),\
+                                    'business_id')
 
     #4) Break down reviews into sentences
-    reviews_df = reviews_df.withColumn('sentences', get_sentence_tokens(reviews_df['contents']))
-    reviews_df = reviews_df.withColumn('sentence_items', functions.split(reviews_df['sentences'], ";"))
+    reviews_df = reviews_df.withColumn('sentences', \
+                                        get_sentence_tokens(reviews_df['contents']))
+
+    reviews_df = reviews_df.withColumn('sentence_items', \
+                                        functions.split(reviews_df['sentences'], ";"))
 
     # 5) Assign positive, neutral and negative sentiment to each sentence
-    sentence_table = reviews_df.select(reviews_df['rating'], functions.explode(reviews_df['sentence_items']).alias('sentences')).cache()
-    sentence_table = sentence_table.withColumn('sentiment_score', get_sentence_sentiments(sentence_table['sentences']))
+    sentence_table = reviews_df.select(reviews_df['business_id'], \
+                                       reviews_df['rating'], \
+                                       functions.explode(reviews_df['sentence_items']).alias('sentences')).cache()
+
+    sentence_table = sentence_table.withColumn('sentiment_score', \
+                                               get_sentence_sentiments(sentence_table['sentences'])).cache()
 
 
     # Clean sentences and generate ngrams using spark nlp and pipeline features
@@ -91,8 +130,7 @@ def main(reviews_file, business_id, ngrams, outdir):
     .setOutputCol('tokenized')
 
 
-    # .setCleanupPatterns
-    # Remove symbols and character
+    # Remove symbols and characters
     normalizer = Normalizer() \
     .setInputCols(['tokenized']) \
     .setOutputCol('normalized') \
@@ -100,12 +138,12 @@ def main(reviews_file, business_id, ngrams, outdir):
 
 
     # Create base froms of words so that different forms of the same word
-    # are grouped as one
+    # are grouped as one single word
     lemmatizer = LemmatizerModel.pretrained() \
     .setInputCols(['normalized']) \
     .setOutputCol('lemmatized')
 
-    # Clean stop words
+    # Clean stop words (common english word)
     stopwords_cleaner = StopWordsCleaner() \
     .setInputCols(['lemmatized']) \
     .setOutputCol('no_stop_lemmatize') \
@@ -116,6 +154,7 @@ def main(reviews_file, business_id, ngrams, outdir):
     .setInputCols(['no_stop_lemmatize']) \
     .setOutputCol("nGrams") \
     .setN(int(ngrams))
+
 
     # convert token column into human readable form
     finisher = Finisher() \
@@ -132,68 +171,20 @@ def main(reviews_file, business_id, ngrams, outdir):
              n_grams_generator,
              finisher])
 
-    output_table = pipeline.fit(sentence_table).transform(sentence_table)
+    # Run Pipeline
+    output_table = pipeline.fit(sentence_table).transform(sentence_table).cache()
 
-    ngrams_table = output_table.select(output_table['rating'], output_table['sentiment_score'], functions.explode(output_table['finished_nGrams']).alias('nGrams')).cache()
-
+    # create a row for each ngram
+    ngrams_table = output_table.select(output_table['business_id'], \
+                                       output_table['rating'], \
+                                       output_table['sentiment_score'], \
+                                       functions.explode(output_table['finished_nGrams']).alias('nGrams')).cache()
 
     print('Generating ngram count table...')
-    n_grams_count = ngrams_table.groupBy('rating', 'sentiment_score', 'nGrams').count()
-    n_grams_count = n_grams_count.sort(n_grams_count['count'].desc())
+    n_grams_count = ngrams_table.groupBy('business_id', 'rating', 'sentiment_score', 'nGrams').count()
+    n_grams_count = n_grams_count.coalesce(1)
 
-
-    print('Generating plots...')
-    # Select top 20 ngrams in each category
-    # Negative Reviews
-
-    # ngrams in negative reviews coming from sentences that have a positive sentiment
-    negative_reviews_pos = n_grams_count.filter((n_grams_count['rating'] == 0) & (n_grams_count['sentiment_score'] == 1))
-    # ngrams in negative reviews coming from sentences that have a negative sentiment
-    negative_reviews_neg = n_grams_count.filter((n_grams_count['rating'] == 0) & (n_grams_count['sentiment_score'] == 0))
-
-    top20_neg_pos = spark.createDataFrame(negative_reviews_pos.head(20))
-    # top20_neg.show()
-    top20_neg_pos = top20_neg_pos.select(top20_neg_pos['nGrams'], top20_neg_pos['count']).toPandas()
-    plt.figure(figsize=(10, 5))
-    plt.barh(top20_neg_pos['nGrams'], top20_neg_pos['count'])
-    plt.savefig(outdir + f'/{business_id}_negative_reviews_histogram_pos_{ngrams}')
-    print(top20_neg_pos.head())
-
-
-    plt.clf()
-    plt.figure(figsize=(10, 5))
-    top20_neg_neg= spark.createDataFrame(negative_reviews_neg.head(20))
-    top20_neg_neg = top20_neg_neg.select(top20_neg_neg['nGrams'], top20_neg_neg['count']).toPandas()
-    print(top20_neg_neg.head())
-    plt.barh(top20_neg_neg['nGrams'], top20_neg_neg['count'])
-    plt.savefig(outdir + f'/{business_id}_negative_reviews_histogram_neg_{ngrams}')
-
-
-
-    # Positive Reviews
-    # ngrams in positive reviews coming from sentences that have a positive sentiment
-    positive_reviews_pos = n_grams_count.filter((n_grams_count['rating'] == 1) & (n_grams_count['sentiment_score'] == 1))
-    # ngrams in positive reviews coming from sentences that have a negative sentiment
-    positive_reviews_neg = n_grams_count.filter((n_grams_count['rating'] == 1) & (n_grams_count['sentiment_score'] == 0))
-
-    top20_pos_pos = spark.createDataFrame(positive_reviews_pos.head(20))
-    # top20_neg.show()
-    top20_pos_pos = top20_pos_pos.select(top20_pos_pos['nGrams'], top20_pos_pos['count']).toPandas()
-    plt.figure(figsize=(10, 5))
-    plt.barh(top20_pos_pos['nGrams'], top20_pos_pos['count'])
-    plt.savefig(outdir + f'/{business_id}_positive_reviews_histogram_pos_{ngrams}')
-    print(top20_pos_pos.head())
-
-
-    plt.clf()
-    plt.figure(figsize=(10, 5))
-    top20_pos_neg= spark.createDataFrame(positive_reviews_neg.head(20))
-    top20_pos_neg = top20_pos_neg.select(top20_pos_neg['nGrams'], top20_pos_neg['count']).toPandas()
-    print(top20_pos_neg.head())
-    plt.barh(top20_pos_neg['nGrams'], top20_pos_neg['count'])
-    plt.savefig(outdir + f'/{business_id}_positive_reviews_histogram_neg_{ngrams}')
-    print('program finished')
-
+    n_grams_count.write.mode("overwrite").csv(outdir + '/n_grams_count.csv', header='true')
 
 if __name__ == '__main__':
     reviews_file = sys.argv[1]
